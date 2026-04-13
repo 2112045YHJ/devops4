cc21b6093d7b639c16468341b641404a72ee2671
2112045YHJ <yuhyunjun08@gmail.com>
2026-04-13 16:37:38 +0900

    Remove '/api/board' prefix from API endpoints

--- my-board-paging/src/api/boardApi.ts
+++ my-board-paging/src/api/boardApi.ts
@@ -1,26 +1,26 @@
 import axiosInstance from './axiosInstance';
 
 export const getBoardList = () =>
-  axiosInstance.get('/api/board/list');
+  axiosInstance.get('/list');
 
 export const getBoardListPaging = (pageNum: number, amount = 10) =>
-  axiosInstance.get('/api/board/listPaging', { params: { pageNum, amount } });
+  axiosInstance.get('/listPaging', { params: { pageNum, amount } });
 
 export const getBoardDetail = (boardId: string | number) =>
-  axiosInstance.get(`/api/board/detail/${boardId}`);
+  axiosInstance.get(`/detail/${boardId}`);
 
 export const insertBoard = (formData: FormData) =>
-  axiosInstance.post('/api/board/insert', formData, {
+  axiosInstance.post('/insert', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 export const updateBoard = (formData: FormData) =>
-  axiosInstance.post('/api/board/update', formData, {
+  axiosInstance.post('/update', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 export const deleteBoard = (boardId: string | number) =>
-  axiosInstance.delete(`/api/board/delete/${boardId}`);
+  axiosInstance.delete(`/delete/${boardId}`);
 
 export const deleteFile = (fileIdx: number) =>
-  axiosInstance.delete('/api/board/deleteFile', { params: { fileIdx } });
+  axiosInstance.delete('/deleteFile', { params: { fileIdx } });
